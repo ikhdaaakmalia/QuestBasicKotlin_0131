afcb421f09254a34663790f5bad65ee7e91674e9
ikhdaakmalia <punyaamelaja@gmail.com>
2024-10-11 22:46:26 +0700

    first commit

--- app/src/main/java/com/example/TipeData.kt
+++ app/src/main/java/com/example/TipeData.kt
@@ -1,2 +1,34 @@
 package com.example
 
+fun main(){
+    //byte
+    val byteValue: Byte = 10
+    println("Byte: $byteValue")
+
+    //int
+    val intValue: Int = 30
+    println("Int: $intValue")
+
+    //float
+    val floatValue: Float = 50.6f
+    println("Float: $floatValue")
+
+    //double
+    val doubleValue: Double = 60.6
+    println("Double:$doubleValue")
+
+    //char
+    val charValue: Char = 'A'
+    println("Char: $charValue")
+
+    //bollean
+    var booleanValue: Boolean = true
+    println("Boolean: $booleanValue")
+
+    //string
+    var stringValue: String = " Hello, World!"
+    println("string: $stringValue")
+
+    stringValue = "Hwllo, World!"
+    println("String: $stringValue")
+}
